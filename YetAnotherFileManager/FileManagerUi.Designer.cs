06d9783645a61558b2f970b767cbf578f5965b1c
SirRoyalPenguin <denisplaysgames@gmail.com>
2016-11-11 21:17:17 +0300

    Added listviews

--- YetAnotherFileManager/FileManagerUi.Designer.cs
+++ YetAnotherFileManager/FileManagerUi.Designer.cs
@@ -28,11 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileManagerUi));
             this.topToolStrip = new System.Windows.Forms.ToolStrip();
-            this.toolStripButtonHelp = new System.Windows.Forms.ToolStripButton();
             this.toolStripButtonAbout = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonHelp = new System.Windows.Forms.ToolStripButton();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listViewLeft = new System.Windows.Forms.ListView();
+            this.NameLeft = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TypeLeft = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.LastModifiedLeft = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.textBoxDirectoryPathLeft = new System.Windows.Forms.TextBox();
+            this.flowPanelLeftDisks = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBoxDirectoryPathRight = new System.Windows.Forms.TextBox();
+            this.flowPanelRightDisks = new System.Windows.Forms.FlowLayoutPanel();
+            this.listViewRight = new System.Windows.Forms.ListView();
+            this.NameRight = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TypeRight = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.LastModified = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.topToolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // topToolStrip
@@ -44,18 +63,9 @@
             this.topToolStrip.Name = "topToolStrip";
             this.topToolStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
             this.topToolStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.topToolStrip.Size = new System.Drawing.Size(284, 25);
+            this.topToolStrip.Size = new System.Drawing.Size(645, 25);
             this.topToolStrip.TabIndex = 0;
             this.topToolStrip.Text = "toolStrip1";
-            // 
-            // toolStripButtonHelp
-            // 
-            this.toolStripButtonHelp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.toolStripButtonHelp.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonHelp.Image")));
-            this.toolStripButtonHelp.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButtonHelp.Name = "toolStripButtonHelp";
-            this.toolStripButtonHelp.Size = new System.Drawing.Size(36, 22);
-            this.toolStripButtonHelp.Text = "Help";
             // 
             // toolStripButtonAbout
             // 
@@ -67,16 +77,153 @@
             this.toolStripButtonAbout.Text = "About";
             this.toolStripButtonAbout.ToolTipText = "About";
             // 
+            // toolStripButtonHelp
+            // 
+            this.toolStripButtonHelp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonHelp.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonHelp.Image")));
+            this.toolStripButtonHelp.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonHelp.Name = "toolStripButtonHelp";
+            this.toolStripButtonHelp.Size = new System.Drawing.Size(36, 22);
+            this.toolStripButtonHelp.Text = "Help";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listViewLeft);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxDirectoryPathLeft);
+            this.splitContainer1.Panel1.Controls.Add(this.flowPanelLeftDisks);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listViewRight);
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxDirectoryPathRight);
+            this.splitContainer1.Panel2.Controls.Add(this.flowPanelRightDisks);
+            this.splitContainer1.Size = new System.Drawing.Size(645, 345);
+            this.splitContainer1.SplitterDistance = 302;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // listViewLeft
+            // 
+            this.listViewLeft.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.listViewLeft.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NameLeft,
+            this.TypeLeft,
+            this.LastModifiedLeft});
+            this.listViewLeft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewLeft.FullRowSelect = true;
+            this.listViewLeft.Location = new System.Drawing.Point(0, 74);
+            this.listViewLeft.Name = "listViewLeft";
+            this.listViewLeft.Size = new System.Drawing.Size(302, 271);
+            this.listViewLeft.SmallImageList = this.imageList1;
+            this.listViewLeft.TabIndex = 2;
+            this.listViewLeft.UseCompatibleStateImageBehavior = false;
+            this.listViewLeft.View = System.Windows.Forms.View.Details;
+            // 
+            // NameLeft
+            // 
+            this.NameLeft.Text = "Name";
+            this.NameLeft.Width = 40;
+            // 
+            // TypeLeft
+            // 
+            this.TypeLeft.Text = "Type";
+            this.TypeLeft.Width = 36;
+            // 
+            // LastModifiedLeft
+            // 
+            this.LastModifiedLeft.Text = "Last Modified";
+            this.LastModifiedLeft.Width = 222;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "folder-close.png");
+            this.imageList1.Images.SetKeyName(1, "file.png");
+            // 
+            // textBoxDirectoryPathLeft
+            // 
+            this.textBoxDirectoryPathLeft.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxDirectoryPathLeft.Enabled = false;
+            this.textBoxDirectoryPathLeft.Location = new System.Drawing.Point(0, 54);
+            this.textBoxDirectoryPathLeft.Name = "textBoxDirectoryPathLeft";
+            this.textBoxDirectoryPathLeft.Size = new System.Drawing.Size(302, 20);
+            this.textBoxDirectoryPathLeft.TabIndex = 1;
+            // 
+            // flowPanelLeftDisks
+            // 
+            this.flowPanelLeftDisks.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowPanelLeftDisks.Location = new System.Drawing.Point(0, 0);
+            this.flowPanelLeftDisks.Name = "flowPanelLeftDisks";
+            this.flowPanelLeftDisks.Size = new System.Drawing.Size(302, 54);
+            this.flowPanelLeftDisks.TabIndex = 0;
+            // 
+            // textBoxDirectoryPathRight
+            // 
+            this.textBoxDirectoryPathRight.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxDirectoryPathRight.Enabled = false;
+            this.textBoxDirectoryPathRight.Location = new System.Drawing.Point(0, 54);
+            this.textBoxDirectoryPathRight.Name = "textBoxDirectoryPathRight";
+            this.textBoxDirectoryPathRight.Size = new System.Drawing.Size(339, 20);
+            this.textBoxDirectoryPathRight.TabIndex = 1;
+            // 
+            // flowPanelRightDisks
+            // 
+            this.flowPanelRightDisks.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowPanelRightDisks.Location = new System.Drawing.Point(0, 0);
+            this.flowPanelRightDisks.Name = "flowPanelRightDisks";
+            this.flowPanelRightDisks.Size = new System.Drawing.Size(339, 54);
+            this.flowPanelRightDisks.TabIndex = 0;
+            // 
+            // listViewRight
+            // 
+            this.listViewRight.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NameRight,
+            this.TypeRight,
+            this.LastModified});
+            this.listViewRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewRight.Location = new System.Drawing.Point(0, 74);
+            this.listViewRight.Name = "listViewRight";
+            this.listViewRight.Size = new System.Drawing.Size(339, 271);
+            this.listViewRight.SmallImageList = this.imageList1;
+            this.listViewRight.TabIndex = 2;
+            this.listViewRight.UseCompatibleStateImageBehavior = false;
+            this.listViewRight.View = System.Windows.Forms.View.Details;
+            // 
+            // NameRight
+            // 
+            this.NameRight.Text = "Name";
+            // 
+            // TypeRight
+            // 
+            this.TypeRight.Text = "Type";
+            // 
+            // LastModified
+            // 
+            this.LastModified.Text = "LastModified";
+            // 
             // FileManagerUi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(645, 370);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.topToolStrip);
             this.Name = "FileManagerUi";
             this.Text = "Yet Another File Manager -_-";
             this.topToolStrip.ResumeLayout(false);
             this.topToolStrip.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,6 +234,20 @@
         private System.Windows.Forms.ToolStrip topToolStrip;
         private System.Windows.Forms.ToolStripButton toolStripButtonHelp;
         private System.Windows.Forms.ToolStripButton toolStripButtonAbout;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBoxDirectoryPathLeft;
+        private System.Windows.Forms.FlowLayoutPanel flowPanelLeftDisks;
+        private System.Windows.Forms.FlowLayoutPanel flowPanelRightDisks;
+        private System.Windows.Forms.TextBox textBoxDirectoryPathRight;
+        private System.Windows.Forms.ListView listViewLeft;
+        private System.Windows.Forms.ImageList imageList1;
+        private new System.Windows.Forms.ColumnHeader NameLeft;
+        private System.Windows.Forms.ColumnHeader TypeLeft;
+        private System.Windows.Forms.ColumnHeader LastModifiedLeft;
+        private System.Windows.Forms.ListView listViewRight;
+        private System.Windows.Forms.ColumnHeader NameRight;
+        private System.Windows.Forms.ColumnHeader TypeRight;
+        private System.Windows.Forms.ColumnHeader LastModified;
     }
 }
 
--- YetAnotherFileManager/FileManagerUi.cs
+++ YetAnotherFileManager/FileManagerUi.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,92 @@ namespace YetAnotherFileManager
     {
         public FileManagerUi()
         {
+            this.ResizeEnd += Form_ResizeEnd;
             InitializeComponent();
+            InitDiskButtons();
+        }
+
+        private void InitDiskButtons()
+        {
+            var drives = DriveInfo.GetDrives();
+            var btnFont = new Font("Arial", 8);
+            Func<string, Button> createButton = delegate (string name)
+            {
+                var btn = new Button
+                {
+                    Text = name,
+                    Font = btnFont,
+                    Height = 24,
+                    Width = 32
+                };
+                btn.Click += BtnDisk_Click;
+                return btn;
+            };
+
+            foreach(var drive in drives)
+            {
+                flowPanelLeftDisks.Controls.Add(createButton(drive.Name));
+                flowPanelRightDisks.Controls.Add(createButton(drive.Name));
+            }
+        }
+
+        private void BtnDisk_Click(object sender, EventArgs e)
+        {
+            var btn = (Button)sender;
+            var diskName = (btn).Text;
+            try
+            {
+                var parent = (FlowLayoutPanel)btn.Parent;
+                bool isLeftPanel = parent.Name == "flowPanelLeftDisks";
+                RefreshListView(isLeftPanel, diskName);
+            }
+            catch
+            {
+                MessageBox.Show(string.Format("unable to access disk {0}", diskName));
+            }
+        }
+        private void RefreshListView(bool isLeftPanel, string dirPath)
+        {
+            var listView = isLeftPanel ? listViewLeft : listViewRight;
+            listView.Items.Clear();
+            var dirInfo = new DirectoryInfo(dirPath);
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+            foreach (DirectoryInfo dir in dirInfo.GetDirectories())
+            {
+                item = new ListViewItem(dir.Name, 0);
+                subItems = new ListViewItem.ListViewSubItem[]
+                          {new ListViewItem.ListViewSubItem(item, "Directory"),
+                   new ListViewItem.ListViewSubItem(item,
+                dir.LastAccessTime.ToShortDateString())};
+                item.SubItems.AddRange(subItems);
+                listView.Items.Add(item);
+            }
+            foreach (FileInfo file in dirInfo.GetFiles())
+            {
+                item = new ListViewItem(file.Name, 1);
+                subItems = new ListViewItem.ListViewSubItem[]
+                          { new ListViewItem.ListViewSubItem(item, "File"),
+                   new ListViewItem.ListViewSubItem(item,
+                file.LastAccessTime.ToShortDateString())};
+
+                item.SubItems.AddRange(subItems);
+                listView.Items.Add(item);
+            }
+
+            ResizeColumnHeaders();
+
+        }
+
+        private void Form_ResizeEnd(object sender, EventArgs e)
+        {
+            ResizeColumnHeaders();
+        }
+
+        private void ResizeColumnHeaders()
+        {
+            listViewLeft.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            listViewRight.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
         }
     }
 }
