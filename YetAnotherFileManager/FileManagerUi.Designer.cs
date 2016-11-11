59af43d517980ae989866c398ad763e5fe20a04f
SirRoyalPenguin <denisplaysgames@gmail.com>
2016-11-11 19:50:23 +0300

    help buttons

--- YetAnotherFileManager/FileManagerUi.Designer.cs
+++ YetAnotherFileManager/FileManagerUi.Designer.cs
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileManagerUi));
+            this.topToolStrip = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonHelp = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonAbout = new System.Windows.Forms.ToolStripButton();
+            this.topToolStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // topToolStrip
+            // 
+            this.topToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonAbout,
+            this.toolStripButtonHelp});
+            this.topToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.topToolStrip.Name = "topToolStrip";
+            this.topToolStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.topToolStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.topToolStrip.Size = new System.Drawing.Size(284, 25);
+            this.topToolStrip.TabIndex = 0;
+            this.topToolStrip.Text = "toolStrip1";
+            // 
+            // toolStripButtonHelp
+            // 
+            this.toolStripButtonHelp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonHelp.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonHelp.Image")));
+            this.toolStripButtonHelp.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonHelp.Name = "toolStripButtonHelp";
+            this.toolStripButtonHelp.Size = new System.Drawing.Size(36, 22);
+            this.toolStripButtonHelp.Text = "Help";
+            // 
+            // toolStripButtonAbout
+            // 
+            this.toolStripButtonAbout.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonAbout.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonAbout.Image")));
+            this.toolStripButtonAbout.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonAbout.Name = "toolStripButtonAbout";
+            this.toolStripButtonAbout.Size = new System.Drawing.Size(44, 22);
+            this.toolStripButtonAbout.Text = "About";
+            this.toolStripButtonAbout.ToolTipText = "About";
+            // 
+            // FileManagerUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.topToolStrip);
+            this.Name = "FileManagerUi";
+            this.Text = "Yet Another File Manager -_-";
+            this.topToolStrip.ResumeLayout(false);
+            this.topToolStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip topToolStrip;
+        private System.Windows.Forms.ToolStripButton toolStripButtonHelp;
+        private System.Windows.Forms.ToolStripButton toolStripButtonAbout;
     }
 }
 
